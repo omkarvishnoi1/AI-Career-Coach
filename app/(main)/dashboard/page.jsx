f2b53edfff639ce908ef1b7427a2e3bfcddfb7ed
omkarvishnoi1 <omkarvishnoi9@gmail.com>
2025-04-27 12:44:52 +0530

    completed

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,19 @@
+import { getIndustryInsights } from "@/actions/dashboard";
+import {getUserOnboardingStatus} from "@/actions/user";
+import DashboardView from "./_component/dashboard-view";
+import { redirect } from "next/navigation";
+
+const IndustryInsightsPage = async () => {
+  const { isOnboarded } = await getUserOnboardingStatus();
+  const insights = await getIndustryInsights();
+  if(!isOnboarded) {
+    redirect("/onboarding");
+  }
+  return(
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
+}
+
+export default IndustryInsightsPage;
